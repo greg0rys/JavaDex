9c297aa8d828cb62ac41d93e79e5417b48842a49
greg0rys <gshenefel@greg0rys.codes>
2024-01-03 16:44:26 -0800

    testing sprite calls

--- src/Data/HTTP_REQUEST.java
+++ src/Data/HTTP_REQUEST.java
@@ -1,7 +1,9 @@
 package Data;
 
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.*;
 
 import static java.lang.System.out;
@@ -11,10 +13,10 @@ public class HTTP_REQUEST
 
     private final String POKEAPI_SPRITE_URL = "https://raw.githubusercontent" +
             ".com/PokeAPI/sprites/master/sprites/pokemon/";
-    private final String sprite_type = ".png";
-    private URL poke_query;
-    private HttpURLConnection connection;
-    private int pkmn_number;
+    private final String sprite_type = ".png"; // image file type to concat to url
+    private URL poke_query; // the URL object that represents a full query
+    private HttpURLConnection connection; // the http connection
+    private int pkmn_number; // pokemons number in dex
 
     public HTTP_REQUEST(){}
     public HTTP_REQUEST(int num) throws IOException
@@ -46,6 +48,20 @@ public class HTTP_REQUEST
     public String get_URL()
     {
         return POKEAPI_SPRITE_URL + pkmn_number + sprite_type;
+    }
+
+    public StringBuffer read_res() throws IOException
+    {
+        BufferedReader in =
+                new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+        StringBuffer res = new StringBuffer();
+
+        while((inputLine = in.readLine()) != null)
+            res.append(inputLine);
+        in.close();
+
+        return res;
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import Data.HTTP_REQUEST;
 
-import java.io.IOException;
+import java.io.*;
+import java.net.URL;
 
 import static java.lang.System.out;
 
@@ -8,8 +9,34 @@ public class Main
 {
     public static void main( String[] args ) throws IOException
     {
+        String output = "ditto.jpg";
 
-        HTTP_REQUEST t = new HTTP_REQUEST(132);
+        HTTP_REQUEST t = new HTTP_REQUEST(350);
         out.println(t.get_URL());
+        StringBuffer res = t.read_res();
+        out.println(res);
+        save_image(t, output);
+    }
+
+    /**
+     * Save the image from the sprite API request
+     * @param rq
+     * @param dest
+     * @throws IOException
+     */
+    public static void save_image(HTTP_REQUEST rq, String dest) throws IOException
+    {
+        URL url = new URL(rq.get_URL());
+        InputStream is = url.openStream();
+        OutputStream os = new FileOutputStream(dest);
+
+        byte[] b = new byte[2048];
+        int length = 0;
+
+        while ((length = is.read(b)) != -1)
+            os.write(b,0,length);
+
+        is.close();
+        os.close();
     }
 }
